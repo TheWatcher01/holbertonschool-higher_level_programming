b2334e90e0f09b50c4997dc90983fc36fe400f83
TheWatcher01 <7450@holbertonstudents.com>
2024-03-18 14:33:54 +0100

    Add 0-privileges.sql

---
+++ SQL_more_queries/0-privileges.sql
@@ -0,0 +1,5 @@
+-- Display the privileges for the user 'user_0d_1' on 'localhost'
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+
+-- Display the privileges for the user 'user_0d_2' on 'localhost'
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
