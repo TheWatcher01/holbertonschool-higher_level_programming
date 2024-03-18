2616d15416083ba5df47eafd38fa1ddbfd4384cb
TheWatcher01 <7450@holbertonstudents.com>
2024-03-18 13:00:44 +0100

    Add 16-no_link.sql

---
+++ SQL_introduction/16-no_link.sql
@@ -0,0 +1,2 @@
+-- Selects and displays the 'score' and 'name' from 'second_table' where 'name' is not null, ordered by 'score' in descending order
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
