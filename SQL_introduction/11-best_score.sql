8a3ef765d7b92dca5733f8718fd6ad768e69da61
TheWatcher01 <7450@holbertonstudents.com>
2024-03-18 12:51:35 +0100

    Add 11-best_score.sql

---
+++ SQL_introduction/11-best_score.sql
@@ -0,0 +1,2 @@
+-- Selects and displays the 'score' and 'name' from 'second_table' where 'score' is greater than or equal to 10, ordered by 'score' in descending order
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
