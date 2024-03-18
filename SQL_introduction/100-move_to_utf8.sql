a4fdfea345edb560828f1231183b204c0d3f4f67
TheWatcher01 <7450@holbertonstudents.com>
2024-03-18 13:06:55 +0100

    Fix 100-move_to_utf8.sql (remove use db)

--- SQL_introduction/100-move_to_utf8.sql
+++ SQL_introduction/100-move_to_utf8.sql
@@ -1,6 +1,3 @@
--- Select the database
-USE `hbtn_0c_0`;
-
 -- Alters the database 'hbtn_0c_0' to use the 'utf8mb4' character set and 'utf8mb4_unicode_ci' collation
 ALTER DATABASE `hbtn_0c_0` CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
 
