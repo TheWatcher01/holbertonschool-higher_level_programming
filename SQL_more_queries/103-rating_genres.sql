54c20049765d98cb67e1e2cdcfe807fb7923b6ba
TheWatcher01 <7450@holbertonstudents.com>
2024-03-20 14:40:52 +0100

    Test 103 again

--- SQL_more_queries/103-rating_genres.sql
+++ SQL_more_queries/103-rating_genres.sql
@@ -1,13 +1,13 @@
--- Select the name of each genre and the sum of its ratings
-SELECT tv_genres.name, SUM(tv_show_ratings.rate) AS rating_sum
+-- Selects the name of each genre and the sum of its ratings
+SELECT tv_genres.name, SUM(tv_show_ratings.rate) AS rating
 FROM
     tv_genres
-    -- Join with the tv_show_genres table where the genre IDs match
-    INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-    -- Join with the tv_show_ratings table where the show IDs match
-    INNER JOIN tv_show_ratings ON tv_show_genres.show_id = tv_show_ratings.show_id
-    -- Group the results by the name of the genre
+    -- Joins with the 'tv_show_genres' table where the genre ids match
+    JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+    -- Joins with the 'tv_show_ratings' table where the show ids match
+    JOIN tv_show_ratings ON tv_show_genres.show_id = tv_show_ratings.show_id
+    -- Groups the results by the genre name
 GROUP BY
     tv_genres.name
-    -- Order the results in descending order by the sum of the ratings
-ORDER BY rating_sum DESC;
+    -- Orders the results in descending order by the sum of ratings
+ORDER BY rating DESC;
