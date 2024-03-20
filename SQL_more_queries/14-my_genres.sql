06ab13e7fd01873bed205f6adbc8f8760e4dd1cc
TheWatcher01 <7450@holbertonstudents.com>
2024-03-20 10:39:06 +0100

    Backup 14-my_genres.sql

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -3,10 +3,10 @@ SELECT tv_genres.name
     -- From 'tv_shows' table
 FROM
     tv_shows
-    /* Inner join 'tv_show_genres' table on 'id' in 'tv_shows' table and 'show_id'
+    /* Inner join 'tv_show_genres' table on 'id' in 'tv_shows' table & 'show_id'
     in 'tv_show_genres' table */
     INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-    -- Inner join 'tv_genres' table on 'genre_id' in 'tv_show_genres' table and 'id' in 'tv_genres' table
+    -- Inner join 'tv_genres' table on 'genre_id' in 'tv_show_genres' table & 'id' in 'tv_genres' table
     INNER JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
     -- Where 'title' in 'tv_shows' table is 'Dexter'
 WHERE
