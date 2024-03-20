7c8755f1aac924097fa079f556cebac7416548e6
TheWatcher01 <7450@holbertonstudents.com>
2024-03-20 12:58:20 +0100

    Refactor genre selection query in 100-not_my_genres.sql

--- SQL_more_queries/100-not_my_genres.sql
+++ SQL_more_queries/100-not_my_genres.sql
@@ -1,17 +1,20 @@
 -- Select the name of the genre from the 'tv_genres' table
-SELECT tv_genres.name
+SELECT name
 FROM tv_genres
-    -- Only select those genres whose 'id' is not in the following subquery
 WHERE
-    tv_genres.id NOT IN(
-        -- This subquery selects the 'genre_id' from the 'show_genres' table
-        SELECT show_genres.genre_id
-            -- It joins 'show_genres' with 'tv_shows' on the matching 'show_id' in 'show_genres' and 'id' in 'tv_shows'
-        FROM show_genres
-            INNER JOIN tv_shows ON show_genres.show_id = tv_shows.id
-            -- Only select those genres linked to the show 'Dexter'
+    -- Only select those genres whose name is not in the following subquery
+    name NOT IN(
+        -- This subquery selects the name of the genre from the 'tv_genres' table
+        SELECT tv_genres.name
+        FROM
+            -- It joins 'tv_genres' with 'tv_show_genres' on the matching 'genre_id' in 'tv_show_genres' & 'id' in 'tv_genres'
+            tv_genres
+            JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id
+            -- It further joins the result with 'tv_shows' on the matching 'show_id' in 'tv_show_genres' & 'id' in 'tv_shows'
+            JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
         WHERE
+            -- Only select those genres linked to the show 'Dexter'
             tv_shows.title = 'Dexter'
     )
     -- Order the results by the genre name in ascending order
-ORDER BY tv_genres.name ASC;
+ORDER BY name ASC;
