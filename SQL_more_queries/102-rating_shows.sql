329410174174af1fc2f203c63462c45de7af7cc7
TheWatcher01 <7450@holbertonstudents.com>
2024-03-20 14:24:20 +0100

    Test checker 102

--- SQL_more_queries/102-rating_shows.sql
+++ SQL_more_queries/102-rating_shows.sql
@@ -1,11 +1,13 @@
--- Select the title of each show and the sum of its ratings
-SELECT tv_shows.title, SUM(tv_show_ratings.rate) AS rating_sum
+-- Select the title of the show and the sum of its ratings
+SELECT CONCAT(
+        tv_shows.title, ' - ', SUM(tv_show_ratings.rating)
+    ) AS `title - rating_sum` -- Concatenate the title and the sum of ratings
 FROM
-    tv_shows -- From the tv_shows table
-    -- Join with the tv_show_ratings table where the show IDs match
+    tv_shows -- From the 'tv_shows' table
+    -- Perform an INNER JOIN with the 'tv_show_ratings' table
+    -- based on the matching 'id' in 'tv_shows' and 'show_id' in 'tv_show_ratings'
     INNER JOIN tv_show_ratings ON tv_shows.id = tv_show_ratings.show_id
-    -- Group the results by the title of the show
 GROUP BY
-    tv_shows.title
-    -- Order the results in descending order by the sum of the ratings
-ORDER BY rating_sum DESC;
+    tv_shows.title -- Group the results by the title of the show
+ORDER BY SUM(tv_show_ratings.rating) DESC;
+-- Order the results by the sum of ratings in descending order
