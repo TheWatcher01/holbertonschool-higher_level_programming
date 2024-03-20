18d45d91706789969e4b54c966cf4a4936469e8c
TheWatcher01 <7450@holbertonstudents.com>
2024-03-20 11:31:38 +0100

    Tests

--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -2,7 +2,7 @@ SELECT tv_shows.title
 FROM
     tv_shows
     INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-    INNER JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
+    INNER JOIN genres ON tv_show_genres.genre_id = genres.id
 WHERE
-    tv_genres.name = "Comedy"
+    genres.name = "Comedy"
 ORDER BY tv_shows.title ASC;
