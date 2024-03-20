4c38efd4d056eae508624faba1de333bcb1ea694
TheWatcher01 <7450@holbertonstudents.com>
2024-03-20 10:47:13 +0100

    Fix 14-my_genres.sql

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,15 +1,15 @@
--- Select 'name' column from 'tv_genres' table
+-- This SQL script lists all genres of the show "Dexter"
+
+-- Select the 'name' column from the 'tv_genres' table
 SELECT tv_genres.name
-    -- From 'tv_shows' table
 FROM
-    tv_shows
-    /* Inner join 'tv_show_genres' table on 'id' in 'tv_shows' table & 'show_id'
-    in 'tv_show_genres' table */
-    INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-    -- Inner join 'tv_genres' table on 'genre_id' in 'tv_show_genres' table & 'id' in 'tv_genres' table
-    INNER JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
-    -- Where 'title' in 'tv_shows' table is 'Dexter'
+    -- Join 'tv_genres' table with 'tv_show_genres' table on 'id' and 'genre_id' columns respectively
+    tv_genres
+    INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+    -- Further join the result with 'tv_shows' table on 'id' and 'show_id' columns respectively
+    INNER JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
 WHERE
-    tv_shows.title = 'Dexter'
-    -- Order results by 'name' in 'tv_genres' table in ascending order
+    -- Filter the results where the 'title' in the 'tv_shows' table is 'Dexter'
+    tv_shows.title = "Dexter"
+    -- Order the results by the 'name' column in the 'tv_genres' table in ascending order
 ORDER BY tv_genres.name ASC;
