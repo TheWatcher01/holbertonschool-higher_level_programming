54858a70ca08d57df9ca34287e3d43ea50256ad3
TheWatcher01 <7450@holbertonstudents.com>
2024-03-20 10:37:20 +0100

    Fix 14-my_genres.sql

--- SQL_more_queries/102-rating_shows.sql
+++ SQL_more_queries/102-rating_shows.sql
@@ -1,9 +1,12 @@
--- Select the title of each show and the sum of its ratings
+-- Select 'title' column from 'tv_shows' table & sum of 'rating' column from 'tv_show_ratings' table
 SELECT tv_shows.title, SUM(tv_show_ratings.rating) AS rating_sum
-FROM tv_shows -- From the tv_shows table
--- Join with the tv_show_ratings table where the show IDs match
-INNER JOIN tv_show_ratings ON tv_shows.id = tv_show_ratings.show_id
--- Group the results by the title of the show
-GROUP BY tv_shows.title
--- Order the results in descending order by the sum of the ratings
+    -- From 'tv_shows' table
+FROM tv_shows
+    /* Inner join 'tv_show_ratings' table on 'id' in 'tv_shows' table &
+    'show_id' in 'tv_show_ratings' table */
+    INNER JOIN tv_show_ratings ON tv_shows.id = tv_show_ratings.show_id
+    -- Group results by 'title' in 'tv_shows' table
+GROUP BY
+    tv_shows.title
+    -- Order results by sum of 'rating' in descending order
 ORDER BY rating_sum DESC;
