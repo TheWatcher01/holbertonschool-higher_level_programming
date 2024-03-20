3fc49a190ba76079f37d0515caad6391f5f1f1e4
TheWatcher01 <7450@holbertonstudents.com>
2024-03-20 11:58:37 +0100

    test

--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -1,8 +1,8 @@
 SELECT tv_shows.title
 FROM
-    tv_shows
-    JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-    JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+    tv_shows_genres
+    INNER JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+    INNER JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
 WHERE
     tv_genres.name = "Comedy"
-ORDER BY tv_shows.title;
+ORDER BY tv_shows.title ASC;
