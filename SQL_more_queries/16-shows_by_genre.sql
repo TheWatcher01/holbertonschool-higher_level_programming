944754d7debd73b0636050757a75a34df1d59ac8
TheWatcher01 <7450@holbertonstudents.com>
2024-03-20 12:25:45 +0100

    Test fix 16-shows_by_genre.sql

--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -1,14 +1,14 @@
--- Select title of show & name of genre. If genre is not available, display 'NULL'
-SELECT tv_shows.title, tv_genres.name AS genre
-    -- From table 'tv_shows'
+/* Select 'title' column from 'tv_shows' table & 'name' column from 'tv_genres' table.
+If genre is not available, display 'NULL'. */
+SELECT tv_shows.title, tv_genres.name
+    -- From 'tv_shows' table
 FROM
     tv_shows
-    /* Perform a LEFT JOIN with 'show_genres' table based on matching 'id' in 'tv_shows'
-    & 'show_id' in 'show_genres'. This ensures that all shows are included,
-    even if they don't have a genre. */
-    LEFT JOIN show_genres ON tv_shows.id = show_genres.show_id
-    /* Perform another LEFT JOIN with 'tv_genres' table based on matching 'genre_id'
-    in 'show_genres' & 'id' in 'tv_genres' */
-    This connects genre names to shows.LEFT JOIN tv_genres ON show_genres.genre_id = tv_genres.id
+    /* Perform LEFT JOIN with 'tv_show_genres' table based on matching 'id' in 'tv_shows' table &
+    'show_id' in 'tv_show_genres'. This ensures that all shows are included, even if they don't have a genre. */
+    LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+    /* Perform another LEFT JOIN with 'tv_genres' table based on matching 'genre_id' in 'tv_show_genres' table
+    & 'id' in 'tv_genres'. This connects genre names to shows. */
+    LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
     -- Order results first by title of show in ascending order, & then by genre name in ascending order.
-ORDER BY tv_shows.title ASC, genre ASC;
+ORDER BY tv_shows.title ASC, tv_genres.name ASC;
