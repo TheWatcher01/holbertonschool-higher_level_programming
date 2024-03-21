763cd3141a917d4cf8880cba53097b63c0817786
TheWatcher01 <7450@holbertonstudents.com>
2024-03-22 00:20:30 +0100

    Update module name and import statement in relationship_city.py

--- python-object_relational_mapping/relationship_city.py
+++ python-object_relational_mapping/relationship_city.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Module: model_city.py
+Module: relationship_city.py
 Author: TheWatcher01
 Date: 21/03/2024
 Description:
@@ -8,25 +8,11 @@ Python file that contains the class definition of a City.
 """
 
 from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.ext.declarative import declarative_base
-
-Base = declarative_base()
+from relationship_state import Base
 
 
 class City(Base):
-    """
-    Represents a city for a MySQL database.
-
-    Attributes:
-        id (int): an auto-generated, unique integer
-                  representing the city's ID.
-        name (str): a string with a maximum of
-                    128 characters representing the city's name.
-        state_id (int): an integer representing the state's ID
-                        that the city belongs to.
-    """
     __tablename__ = 'cities'
-    id = Column(Integer, primary_key=True, nullable=False)
+    id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey(
-        'states.id', ondelete="CASCADE"), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'))
