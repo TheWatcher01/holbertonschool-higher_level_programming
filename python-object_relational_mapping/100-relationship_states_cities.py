3b0a719e652996e50d4e392c1996e0ff5886e14c
TheWatcher01 <7450@holbertonstudents.com>
2024-03-22 00:32:11 +0100

    Test for checker

--- python-object_relational_mapping/100-relationship_states_cities.py
+++ python-object_relational_mapping/100-relationship_states_cities.py
@@ -1,62 +1,28 @@
 #!/usr/bin/python3
+"""Start link class to table in database
 """
-Module: 100-relationship_states_cities.py
-Author: TheWatcher01
-Date: 21/03/2024
-Description:
-Script creates the State “California” with the City “San Francisco” from the
-database hbtn_0e_100_usa.
-It takes 3 arguments: mysql username, mysql password, and database name.
-Uses SQLAlchemy to connect to MySQL server running on localhost at port 3306.
-The code is not executed when imported.
-"""
-
-from sqlalchemy.orm import sessionmaker
-from Utils.check_MySQL import check_mysql
-from Utils.engine_setup import setup_engine
+from sys import argv
 from relationship_state import Base, State
 from relationship_city import City
-import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 
-def create_state_city():
-    """
-    Function to create the State “California” with the City “San Francisco”.
-    """
-
-    if len(sys.argv) != 4:
-        print("Usage: ./100-relationship_states_cities.py "
-              "username password database")
-        return
-
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-
-    try:
-        engine = setup_engine(username, password, database)
-
-        if engine is None:
-            return
-
-        Session = sessionmaker(bind=engine)
-        session = Session()
-
-        new_state = State(name="California")
-        new_city = City(name="San Francisco", state=new_state)
-
-        session.add(new_state)
-        session.add(new_city)
-        session.commit()
-
-    except Exception as e:
-        print(e)
-
-    finally:
-        if session:
-            session.close()
+def relationship_state():
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
+                           format(argv[1],
+                                  argv[2],
+                                  argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    session = sessionmaker(bind=engine)()
+    c = City(name='San Francisco')
+    s = State(name='California', cities=[c])
+    session.add(s)
+    session.add(c)
+    session.commit()
+    session.close()
 
 
 if __name__ == "__main__":
-    check_mysql()
-    create_state_city()
+    relationship_state()
--- python-object_relational_mapping/relationship_city.py
+++ python-object_relational_mapping/relationship_city.py
@@ -1,34 +1,27 @@
 #!/usr/bin/python3
-"""
-Module: relationship_state.py
-Author: TheWatcher01
-Date: 21/03/2024
-Description:
-Python file that contains the class definition of a State.
-"""
+'''
+Contains the class def of City
+'''
 
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import relationship
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, ForeignKey
+from relationship_state import Base, State
 
 
-Base = declarative_base()
+class City(Base):
+    '''Class def for City'''
 
+    __tablename__ = 'cities'
+    id = Column(Integer,
+                autoincrement=True,
+                unique=True,
+                nullable=False,
+                primary_key=True)
+    name = Column(String(128),
+                  nullable=False)
+    state_id = Column(Integer,
+                      ForeignKey('states.id'),
+                      nullable=False)
 
-class State(Base):
-    """
-    State class:
-    - inherits from Base
-    - links to the MySQL table states
-    - class attribute id that represents a column of an auto-generated,
-      unique integer, can’t be null and is a primary key
-    - class attribute name that represents a column of a string of 128
-      characters and can’t be null
-    - class attribute cities must represent a relationship with the class City
-    """
-
-    __tablename__ = 'states'
-    id = Column(Integer, primary_key=True, nullable=False)
-    name = Column(String(128), nullable=False)
-    cities = relationship("City", back_populates="state",
-                          cascade="all, delete")
+    def __str__(self):
+        '''str form of the class'''
+        return "{}: {}".format(self.id, self.name)
