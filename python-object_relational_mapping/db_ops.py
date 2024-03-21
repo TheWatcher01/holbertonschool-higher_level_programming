530919446498a73b92d44e0d29e1892397706672
TheWatcher01 <7450@holbertonstudents.com>
2024-03-21 18:55:39 +0100

    Add function to list all State objects from the database

--- python-object_relational_mapping/db_ops.py
+++ python-object_relational_mapping/db_ops.py
@@ -1,10 +1,22 @@
+"""
+Module: db_ops.py
+Author: TheWatcher01
+Date: 21/03/2024
+Description:
+Module provides a function to list all State objects from database.
+"""
+
+# Import necessary modules
 from model_state import State
 
 
 def list_states(session):
     """
     Function to list all State objects from the database.
+    Takes 1 argument: session (SQLAlchemy Session object).
+    Prints the id and name of each State object, sorted by id.
     """
+
     # Query the database
     for state in session.query(State).order_by(State.id):
         print("{}: {}".format(state.id, state.name))
