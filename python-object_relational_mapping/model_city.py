f11dfdb2ba4deb1541c16f6555fb36bdff1737f3
TheWatcher01 <7450@holbertonstudents.com>
2024-03-22 00:02:55 +0100

    Refactor City class in model_city.py

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -8,23 +8,25 @@ Python file that contains the class definition of a City.
 """
 
 from sqlalchemy import Column, Integer, String, ForeignKey
-from model_state import Base, State
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
 
 
 class City(Base):
     """
-    City class:
-    - inherits from Base (imported from model_state)
-    - links to the MySQL table cities
-    - class attribute id that represents a column of an auto-generated,
-      unique integer, can’t be null and is a primary key
-    - class attribute name that represents a column of a string of 128
-      characters and can’t be null
-    - class attribute state_id that represents a column of an integer,
-      can’t be null and is a foreign key to states.id
-    """
+    Represents a city for a MySQL database.
 
+    Attributes:
+        id (int): an auto-generated, unique integer
+                  representing the city's ID.
+        name (str): a string with a maximum of
+                    128 characters representing the city's name.
+        state_id (int): an integer representing the state's ID
+                        that the city belongs to.
+    """
     __tablename__ = 'cities'
     id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
+    state_id = Column(Integer, ForeignKey(
+        'states.id', ondelete="CASCADE"), nullable=False)
