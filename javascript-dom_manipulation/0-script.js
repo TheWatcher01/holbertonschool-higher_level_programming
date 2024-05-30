631001d58c92f9549906d5146465e8a2b2f7d7d4
TheWatcher01 <7450@holbertonstudents.com>
2024-05-30 10:03:21 +0200

    feat: Update text color of HTML tag HEADER to red (#FF0000)

--- javascript-dom_manipulation/0-script.js
+++ javascript-dom_manipulation/0-script.js
@@ -2,6 +2,11 @@
  * @file 0-script.js
  * @author TheWatcher01
  * @date 30-05-2024
- * @description
+ * @description Script that updates the text color of the HTML tag HEADER to red (#FF0000)
  */
 
+// Get the header element
+const headerElement = document.querySelector('header');
+
+// Change the color of the header element to red
+headerElement.style.color = '#FF0000';
