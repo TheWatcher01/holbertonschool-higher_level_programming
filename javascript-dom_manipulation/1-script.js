5f347fcc56172badf9cbd01b87be7b7f760433c1
TheWatcher01 <7450@holbertonstudents.com>
2024-05-30 10:08:44 +0200

    chore: Update header color to red on button click

--- javascript-dom_manipulation/1-script.js
+++ javascript-dom_manipulation/1-script.js
@@ -5,3 +5,12 @@
  * @description
  */
 
+// Get the header element
+const headerElement = document.querySelector('header');
+
+const redHeaderButton = document.getElementById('red_header');
+
+redHeaderButton.addEventListener('click', function () {
+  // Change the color of the header element to red
+  headerElement.style.color = '#FF0000';
+});
