e56feeece9a0250da6ad8c5ce1ac203e6be7556a
TheWatcher01 <7450@holbertonstudents.com>
2024-05-30 10:28:12 +0200

    feat: Update text color of HTML tag HEADER to red (#FF0000)

--- javascript-dom_manipulation/2-script.js
+++ javascript-dom_manipulation/2-script.js
@@ -2,6 +2,13 @@
  * @file 2-script.js
  * @author TheWatcher01
  * @date 30-05-2024
- * @description
+ * @description Script that updates the text color of the HTML tag HEADER to red (#FF0000) when the user clicks on the tag DIV#red_header
  */
 
+const headerElement = document.querySelector('header');
+
+const redHeaderButton = document.getElementById('red_header');
+
+redHeaderButton.addEventListener('click', function () {
+  headerElement.classList.add('red');
+});
